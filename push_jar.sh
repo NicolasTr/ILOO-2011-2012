add4c13b47e7f03ad0842578a7737518d892e3a5
Nicolas Trésegnie <nicolas.tresegnie@gmail.com>
2012-04-23 00:38:16 +0200

    Correction du script d'upload

--- push_jar.sh
+++ push_jar.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-group_type=$1
-group=$2
-old_hash=`git rev-parse HEAD`
-
 if [ $# -ne 2 ]
 then
     echo "Usage : " 
     echo "   ./push_jar.sh Game groupxx"
     echo "   ./push_jar.sh GUI  groupxx"
+    exit
 fi
 
-git reset
+group_type=$1
+group=$2
+
+# Copie le dépôt distant
+tempdir=`mktemp -d`
+git clone git@github.com:NicolasTr/ILOO-2011-2012.git $tempdir
+
+# Ajoute le .jar
 ant jar
-rm -f lib/$group_type/$group.jar
-cp "build/jar/Projet ILOO 2011-2012.jar" lib/$group_type/$group.jar
-git add lib/$group_type/$group.jar
-git commit -m $group
+cp "build/jar/Projet ILOO 2011-2012.jar" $tempdir/lib/$group_type/$group.jar
 
-new_hash=`git rev-parse HEAD`
-if [ "$old_hash" != "$new_hash" ]
-then
-    git push iloo `git rev-parse HEAD`:master
-fi
+# Effectue le commit
+cd $tempdir
+git add .
+git commit -m $group
+git push
+
+rm -rf $tempdir
